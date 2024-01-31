1bf7e721e57ef42224e0abbd40e48ec867f3fbb7
ANDRES HEREDIA <31019836+andresherediaa@users.noreply.github.com>
2024-01-30 19:10:41 -0500

    Updates

--- src/middlewares/require-auth.ts
+++ src/middlewares/require-auth.ts
@@ -24,11 +24,10 @@ export const currentUser = (
 
   console.log("7777777777", !req.session?.jwt, req.session);
 
-  
+
   if (!req.session?.jwt) {//no se ejcuta si existe jwt
     return next();
   }
-
   try {
     const payload = jwt.verify(
       req.session.jwt,
