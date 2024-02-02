aa02a068b71fa22ebc570b3accd4bbb55449a47c
Carlos Andrés Heredia Álvarez <caheredia@cuenca.gob.ec>
2024-02-02 12:24:33 -0500

    Updates

--- src/errors/bad-request-error.ts
+++ src/errors/bad-request-error.ts
@@ -1,11 +1,11 @@
 import { CustomError } from './custom-error';
 
 export class BadRequestError extends CustomError {
-  statusCode:number;
+  public statusCode:number = 400;
 
   constructor(public message: string, statusCode?: number ) {
     super(message);
-    this.statusCode = statusCode ? this.statusCode : 400 ;
+    this.statusCode = statusCode ? this.statusCode : 400;
     Object.setPrototypeOf(this, BadRequestError.prototype);
   }
 
