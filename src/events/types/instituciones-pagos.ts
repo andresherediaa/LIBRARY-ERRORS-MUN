fd01eec889aed65356fb76a6ee5ffb035f971266
Carlos Andrés Heredia Álvarez <caheredia@cuenca.gob.ec>
2024-03-11 16:44:56 -0500

    Updates

--- src/events/types/instituciones-pagos.ts
+++ src/events/types/instituciones-pagos.ts
@@ -1,19 +1,42 @@
 
 export type CanalTransacionalOptions = "VENT" | "NDAU" | "ATM" | "BVIR" | "BELE" | "IVR" | "BCEL";
+export type EstadoSolPagoType = "ING" | "FPAG" | "INST" | "DEB" | "BANE" | "MPAG" | "SMAN" | "EMI" | "AUT" | "CLIR" | "ECAN";
+export type EntidadExterna = "PIMUNICI";
+export type FormaPago = "EFE";
+export type EstadoPagoType = "PAG" | "TRAN" | "TCOMP" | "ERRING" | "NOTC" | "MON" | "CTA" | "INST" | "AUT" | "REV" | "ECAN";
 
 export interface EtapaSolPagosEntries {
- Ientidadexterna: string,
+ Ientidadexterna: EntidadExterna,
  Icuentabancaria: string,
- Iformapago: string,
+ Iformapago: FormaPago,
  Icanaltransacional: CanalTransacionalOptions,
  Iidentificadordeuda: string,
  Imontopagar: number
 }
-
 export interface EtapaSolPagosOutput {
- oEntidadExterna: string,
- oEstado: string,
+ oEntidadExterna: FormaPago,
+ oEstado: EstadoSolPagoType,
  oNumTransaccion: number
 }
+export interface EtapaConfirmarPagoEntries {
+ Ientidadexterna: EntidadExterna,
+ Icuentabancaria: string,
+ formapago: FormaPago,
+ Icanaltransacional: CanalTransacionalOptions,
+ Iidentificadordeuda: string,
+ Imontopagar: number,
+ Inumtransaccion: number
+}
+export interface EtapaConfirmarPagoOutput {
+ oEntidadExterna: EntidadExterna,
+ oEstado: EstadoPagoType,
+ oNumTransaccion: number,
+ oIdentificadorDeuda: string,
+ oInsCodReferencia: string,
+ oNombresRazonSocial: string,
+ oMensajeCliente: string
+}
+
+
 
 
