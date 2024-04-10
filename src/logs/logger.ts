a8517ff8441b2a286c8ecc8a19fa71c44e598cb4
Carlos Andres Heredia Alvarez <caheredia@mcuenca.pri>
2024-04-10 09:38:55 -0500

    Updates

--- src/logs/logger.ts
+++ src/logs/logger.ts
@@ -1,10 +1,9 @@
 //@ts-ignore
 import { LogService } from "logstash-winston-3";
 
-const logger = LogService.getInstance({
+export const logger = LogService.getInstance({
   serviceName:  process.env.SERVICE_NAME,
   logstashHost: process.env.LOGS_SERVER,
   logstashPort: process.env.LOGS_PORT,
 });
 
-export { logger };
