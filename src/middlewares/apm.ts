260af14703c99d7c22ee62ca8e4d0a534ae9dd3e
Carlos Andres Heredia <caheredia@cuenca.gob.ec>
2024-09-18 16:50:24 -0500

    Updates

--- src/middlewares/apm.ts
+++ src/middlewares/apm.ts
@@ -1,12 +1,12 @@
 const apm = require('elastic-apm-node');
 
-const apmInstance = apm.start({
+const apmInstance = {};/*  apm.start({
  serviceName: process.env.ELK_SERVICE_NAME || 'MUNICIPIO_OBSERVABILIDAD',
  apiKey: process.env.ELK_API_KEY,
  serverUrl: process.env.ELK_SERVER_URL,
  environment: process.env.ELK_ENVIRONMENT || 'DEV',
  logLevel: "error",
-});
+}); */
 
 // Configura el logger
 
