2d46bd498b72b38f540c58269186fa8bcf8aa8d2
Carlos Andres Heredia <caheredia@cuenca.gob.ec>
2024-09-18 16:03:57 -0500

    Updates

--- src/middlewares/apm.ts
+++ src/middlewares/apm.ts
@@ -1,10 +1,35 @@
-// apm.ts
-const apm = require('elastic-apm-node').start({
+const winston = require('winston');
+const LogstashTransport = require('logstash-winston-3');
+const apm = require('elastic-apm-node');
+
+const apmInstance = apm.start({
  serviceName: process.env.ELK_SERVICE_NAME || 'MUNICIPIO_OBSERVABILIDAD',
  apiKey: process.env.ELK_API_KEY,
  serverUrl: process.env.ELK_SERVER_URL,
  environment: process.env.ELK_ENVIRONMENT || 'DEV',
- logLevel: process.env.ELK_LOG_LEVEL || 'trace',
+ logLevel: "error",
 });
 
-export default apm;
+// Configura el logger
+const loggerAPM = winston.createLogger({
+ level: 'info',
+ format: winston.format.json(),
+ transports: [
+  new LogstashTransport({
+   level: 'info',
+   host: process.env.ELK_SERVER_URL,
+   port: 443,
+   ssl: true,
+  }),
+  new winston.transports.Console(),
+ ],
+});
+
+// Asegúrate de que APM capture logs
+loggerAPM.on('data', (log: any) => {
+ console.log("captura el log......");
+ apmInstance.captureError(log);
+});
+
+// Exporta el logger para usarlo en tu aplicación
+export { apmInstance, loggerAPM };
