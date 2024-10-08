d64444d96a55a76723d225aa8f38bd656482767f
Carlos Andres Heredia <caheredia@cuenca.gob.ec>
2024-10-08 15:37:21 -0500

    Updates

--- src/errors/genericError.ts
+++ src/errors/genericError.ts
@@ -5,13 +5,14 @@ export class GenericError extends CustomError {
  statusCode: number;
 
  constructor(
-  public msg: string,         // Mensaje principal del error
+  public msg: string,
+  public httpStatus: number,// Mensaje principal del error
   public code: string = "500", // Código de estado (por defecto 500)
   public typeError: string = "MIDDLEWARE", // Tipo de error opcional
   public userMsg: string = "Hemos encontrado un problema. Inténtelo nuevamente más tarde." // Mensaje amigable para el usuario
  ) {
   super(msg, typeError, userMsg);
-  this.statusCode = 500; // Convertir el código a número para HTTP
+  this.statusCode = this.httpStatus; 
   this.msg = ErrorController.getErrorMessage(code)  || this.message;
   Object.setPrototypeOf(this, GenericError.prototype);
  }
