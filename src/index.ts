43117352e2cdcda6c6390f8e72e8620589374ab1
Carlos Andres Heredia <caheredia@cuenca.gob.ec>
2024-10-09 15:39:34 -0500

    Updates

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,6 @@ export * from './errors/not-authorized-error';
 export * from './errors/not-found-error';
 export * from './errors/request-validation-error';
 export * from './errors/genericError';
-
 //export * from './middlewares/current-user';
 export * from './middlewares/error-handler';
 export * from './middlewares/require-auth';
