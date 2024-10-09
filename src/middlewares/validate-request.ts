4266b5292a2323c67e3cea7ec4cb345354238c7a
Carlos Andres Heredia <caheredia@cuenca.gob.ec>
2024-10-09 15:46:53 -0500

    Updates

--- src/middlewares/validate-request.ts
+++ src/middlewares/validate-request.ts
@@ -2,6 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 import { validationResult } from 'express-validator';
 import { RequestValidationError } from '../errors/request-validation-error';
 
+
 export const validateRequest = (
   req: Request,
   res: Response,
@@ -10,7 +11,13 @@ export const validateRequest = (
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
-    throw new RequestValidationError(errors.array());
+    // Extraer el primer error y sus detalles personalizados (typeError y userMsg)
+    const firstError = errors.array()[0]; // Primer error encontrado
+    const typeError = firstError.msg.typeError || "VALIDATION_ERROR";
+    const userMsg = firstError.msg.userMsg || "Error de validación en la solicitud";
+
+    // Lanzar RequestValidationError con los campos personalizados
+    throw new RequestValidationError(errors.array(), userMsg, typeError);
   }
 
   next();
