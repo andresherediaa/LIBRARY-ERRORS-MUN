07bd8ef93224b88af7074536f86cb65eaac3fe04
Carlos Andres Heredia <caheredia@cuenca.gob.ec>
2024-10-09 15:36:01 -0500

    Updates

--- src/errors/bad-request-error.ts
+++ src/errors/bad-request-error.ts
@@ -1,22 +1,24 @@
+import { ErrorCategories } from '../events/types/instituciones-pagos';
 import { CustomError } from './custom-error';
 import { ErrorController } from './erroresStatus';
 
 export class BadRequestError extends CustomError {
-  public statusCode:number = 400;
+ public statusCode: number = 400;
 
-  constructor(public msg: string, statusCode?: number ) {
-    super(msg, "", "");
-    this.statusCode = statusCode ? this.statusCode : 400;
-    Object.setPrototypeOf(this, BadRequestError.prototype);
-  }
-
-  serializeErrors() {
-   return {
-    status: ErrorController.getGeneralStatus(this.statusCode.toString()),
-    msg: this.message,
-    code: this.statusCode.toString(),
-    typeMsg: this.typeMsg,
-    userMsg: this.userMsg
-   };
-  }
+ constructor(public msg: string, statusCode?: number) {
+  super(msg, "", "");
+  this.statusCode = statusCode ? this.statusCode : 400;
+  this.typeMsg = ErrorCategories.MIDDLEWARE;
+  this.userMsg = ErrorController.getErrorMessage(ErrorCategories.MIDDLEWARE);
+  Object.setPrototypeOf(this, BadRequestError.prototype);
+ }
+ serializeErrors() {
+  return {
+   status: ErrorController.getGeneralStatus(this.statusCode.toString()),
+   msg: this.message,
+   code: this.statusCode.toString(),
+   typeMsg: this.typeMsg,
+   userMsg: this.userMsg
+  };
+ }
 }
