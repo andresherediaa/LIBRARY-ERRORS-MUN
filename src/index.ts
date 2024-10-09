0a67783ec1237cdef4ac2b20cd642516882a3377
Carlos Andres Heredia <caheredia@cuenca.gob.ec>
2024-10-09 15:38:16 -0500

    Updates

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 // Re-export stuff from errors and middlewares
+export * from './errors/bad-request-error';
 export * from './errors/custom-error';
 export * from './errors/database-connection-error';
 export * from './errors/not-authorized-error';
