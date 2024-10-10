de97ff3312aecbd93937b72d507f8b79bc1403b9
Carlos Andres Heredia <caheredia@cuenca.gob.ec>
2024-10-10 10:33:41 -0500

    Updates

--- src/middlewares/validate-request.ts
+++ src/middlewares/validate-request.ts
@@ -17,8 +17,8 @@ export const validateRequest = (
     console.log("VALIDATE REQUEST1111111", errors.array()[0]);
     // Extraer el primer error y sus detalles personalizados (typeError y userMsg)
     const msg = errors.array()[0].msg.msg; // Primer error encontrado
-    const typeMsg = msg.typeMsg ? msg.typeMsg : ErrorCategories.SINTAX;
-    const userMsg = msg.userMsg ? msg.userMsg : ErrorController.getErrorMessage(ErrorCategories.SINTAX);
+    const typeMsg =  ErrorCategories.SINTAX;
+    const userMsg =  ErrorController.getErrorMessage(ErrorCategories.SINTAX);
 
     // Lanzar RequestValidationError con los campos personalizados
     throw new RequestValidationError(msg, typeMsg, userMsg);
